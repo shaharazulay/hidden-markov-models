232d337d0333bad025cd8e1c0b6dc85763704ff8
shaharazulay <shahar4@gmail.com>
2019-07-28 11:55:54 +0300

    fixed memoization issue + added msg normalization

--- hmm/_base.py
+++ hmm/_base.py
@@ -19,15 +19,15 @@ class HMM(object):
     def _validate_propagation_method(self, method):
         if method not in PROPAGATION_METHODS:
             raise ValueError('method not in {}'.format(PROPAGATION_METHODS))
-            
-class Memoize(object):
-    
-    def __init__(self, f):
-        self.f = f
-        self.memo = {}
+
+    def _normalize_msg(self, msg):
+        """ 
+        Normalize a message to avoid growing to infinity or decaying to zero.
         
-    def __call__(self, *args):
-        if not args in self.memo:
-            self.memo[args] = self.f(*args)
-        
-        return self.memo[args]
+        Params:
+        --------
+        msg: dict, the input message (for every possible believed value).
+        """
+        total = sum(msg.values())
+        norm_msg = {k: v / total for k, v in msg.items()}
+        return norm_msg
