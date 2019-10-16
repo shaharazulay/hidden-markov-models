94248365f55ff950ecff5e6de735e1361c07a3a2
shaharazulay <shahar4@gmail.com>
2019-10-16 09:45:48 +0300

    moving to logarithmic max-sim algorithm

--- hmm/_chain_torch.py
+++ hmm/_chain_torch.py
@@ -17,9 +17,9 @@ class Chain(torch.nn.Module):
         beliefs_pred = torch.Tensor()
         for b_idx in range(batch_size):
 
-            values = torch.Tensor([0, 1]) - 0.5
+            values = torch.Tensor([0, 1]) - 0.5 
             pairs = torch.mul(values.view(-1, 2).t(), values)
-            unit_msg = torch.ones([2, 1])
+            unit_msg = torch.zeros([2, 1])
             
             forward_messages = unit_msg
             msg_left = unit_msg # no information traveling left to x1
@@ -27,17 +27,12 @@ class Chain(torch.nn.Module):
             # forward pass    
             for i in range(0, self._length - 1):
                 
-                phi = torch.exp(torch.mul(b[int(observations[b_idx][i])], values))
-                psi = torch.exp(torch.mul(j, pairs))
+                phi = torch.mul(b[int(observations[b_idx][i])], values).view(-1, 1)
+                psi = torch.mul(j, pairs)
       
-                step1 = torch.mul(phi, msg_left.t())
-                step2 = torch.mul(step1, psi)
-                step3, _ = torch.max(step2, dim=1)
-
-                msg = step3.view(-1 ,1)
-                norm_ = torch.norm(msg, p=1, dim=0)  # L1 norm
-                #msg = torch.div(msg, norm_)
-                
+                msg, _ = torch.max(phi + psi + msg_left, dim=0)
+                msg = msg.view(-1 , 1)
+        
                 forward_messages = torch.cat((forward_messages, msg), dim=1)    
                 msg_left = msg
             
@@ -48,32 +43,23 @@ class Chain(torch.nn.Module):
             # backward pass    
             for i in range(self._length - 1, 0, -1):
 
-                phi = torch.exp(torch.mul(b[int(observations[b_idx][i])], values))
-                psi = torch.exp(torch.mul(j, pairs))
-                step1 = torch.mul(phi, msg_right.t())
-                step2 = torch.mul(step1, psi)
-                step3, _ = torch.max(step2, dim=1)
-
-                msg = step3.view(-1 ,1)
-                norm_ = torch.norm(msg, p=1, dim=0)  # L1 norm
-                #msg = torch.div(msg, norm_)
+                phi = torch.mul(b[int(observations[b_idx][i])], values).view(-1, 1)
+                psi = torch.mul(j, pairs)
                 
+                msg, _ = torch.max(phi + psi + msg_right, dim=0)
+                msg = msg.view(-1 , 1)
+        
                 backward_messages = torch.cat((msg, backward_messages), dim=1)    
                 msg_right = msg
 
-            # calculate message propagation
-            messages = torch.mul(forward_messages, backward_messages)
             # add data term
             data_term = torch.Tensor()
             for i in range(0, self._length):
-                phi = torch.exp(torch.mul(b[int(observations[b_idx][i])], values))
+                phi = torch.mul(b[int(observations[b_idx][i])], values)
                 data_term = torch.cat((data_term, phi.view(-1, 1)), dim=1)
             
             # calculate beliefs
-            beliefs = torch.mul(data_term, messages)
-            #norm_ = torch.norm(beliefs, p=1, dim=0)  # L1 norm
-            #beliefs_norm = torch.div(beliefs, norm_)
-            
+            beliefs = data_term + forward_messages + backward_messages            
             beliefs_pred = torch.cat((beliefs_pred, beliefs.expand(1, 2, -1)), dim=0)
         
         return beliefs_pred
